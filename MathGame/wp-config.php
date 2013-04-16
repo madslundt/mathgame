f9c384eec7b9ff388c12bf58bd8d0fa397617a7e
madslundt <madssx@hotmail.com>
2013-04-16 14:07:22 +0200

    database

--- MathGame/wp-config.php
+++ MathGame/wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'MathGame');
 
 /** MySQL database username */
-define('DB_USER', 'xxx');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'xxx');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
