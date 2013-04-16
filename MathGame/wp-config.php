44bfc58b77a72cf3daa090586d6aeb6755867bbf
Mads <madssx@hotmail.com>
2013-04-17 00:56:56 +0200

    Language files for danish translation

--- MathGame/wp-config.php
+++ MathGame/wp-config.php
@@ -69,7 +69,7 @@ $table_prefix  = 'wp_mathgame';
  * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
  * language support.
  */
-define('WPLANG', '');
+define('WPLANG', 'da_DK');
 
 /**
  * For developers: WordPress debugging mode.
