21c3a3825be75499c5f6033a5e863acdcfb11b5a
Mads <madssx@hotmail.com>
2013-06-07 16:11:14 +0200

    Moved game button and removed sidebar

--- MathGame/wp-content/themes/wpbootstrap/template-level-form.php
+++ MathGame/wp-content/themes/wpbootstrap/template-level-form.php
@@ -6,16 +6,13 @@ Template Name: Level form template
 
 <?php get_header(); ?>
 	<div class="row">
-		<div class="span8">
+		<div class="span12">
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 				<legend><?php the_title(); ?></legend>
 				<?php get_template_part('level-form'); ?>
 			<?php endwhile; else: ?>
 				<p><?php _e('Sorry, this page does not exist.'); ?></p>
 			<?php endif; ?>
-		</div>
-		<div class="span4">
-			<?php get_sidebar(); ?>	
 		</div>
 	</div>	
 <?php get_footer(); ?>
